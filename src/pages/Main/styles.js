01b847e9cae3d335f5cadb6e6c3c7fde3ffcdd14
Oscar Broch <brochj@gmail.com>
2019-09-03 19:56:59 -0300

    Estilizando o formulario

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -3,6 +3,7 @@ import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
+  padding: 30px;
 `;
 
 export const Form = styled.View`
@@ -12,12 +13,22 @@ export const Form = styled.View`
   border-color: #eee;
 `;
 
-export const Input = styled.TextInput`
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: '#333',
+})`
   flex: 1;
   height: 40px;
   background: #eee;
   border-radius: 4px;
   padding: 0 15px;
+  border: 1px solid #eee;
 `;
 
-export const SubmitButton = styled(RectButton)``;
+export const SubmitButton = styled(RectButton)`
+  justify-content: center;
+  align-items: center;
+  background: #7159c1;
+  border-radius: 4px;
+  margin-left: 10px;
+  padding: 0 12px;
+`;
