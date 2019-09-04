2e7f9107b82c2723dd915887ea6e9f45601587f9
Oscar Broch <brochj@gmail.com>
2019-09-03 21:13:42 -0300

    Estilizando a listagem de usuarios

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,7 +3,19 @@ import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../services/api';
 
-import { Container, Form, Input, SubmitButton } from './styles';
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText,
+} from './styles';
 
 export default class Main extends React.Component {
   state = {
@@ -13,6 +25,7 @@ export default class Main extends React.Component {
 
   handleAddUser = async () => {
     const { users, newUser } = this.state;
+    if (newUser === '') return;
 
     const response = await api.get(`/users/${newUser}`);
 
@@ -49,6 +62,22 @@ export default class Main extends React.Component {
             <Icon name="add" size={20} color="#fff" />
           </SubmitButton>
         </Form>
+
+        <List
+          data={users}
+          keyExtractor={user => user.login}
+          renderItem={({ item }) => (
+            <User>
+              <Avatar source={{ uri: item.avatar }} />
+              <Name>{item.name}</Name>
+              <Bio>{item.bio}</Bio>
+
+              <ProfileButton onPress={() => {}}>
+                <ProfileButtonText>Ver Perfil</ProfileButtonText>
+              </ProfileButton>
+            </User>
+          )}
+        />
       </Container>
     );
   }
